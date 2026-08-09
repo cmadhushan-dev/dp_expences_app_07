790fdd27486aca3d93ef11ea807bdd11e21395c7
chamodya madhushan <kalupahanamadhushan@gmail.com>
2026-08-09 09:57:31 +0530

    sucfully developed the time and data picker also implimnted the logic for the data time formatting

--- lib/screens/all_screens/add_new_screen.dart
+++ lib/screens/all_screens/add_new_screen.dart
@@ -1,7 +1,8 @@
-import 'package:dp_expenz_application/constant/constatnt.dart';
 import 'package:dp_expenz_application/model/expence_model.dart';
 import 'package:dp_expenz_application/model/income_model.dart';
+import 'package:dp_expenz_application/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../../constant/colors.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -17,6 +18,25 @@ class _AddNewScreenState extends State<AddNewScreen> {
 
   IncomeCategary _incomeCategory = IncomeCategary.passive;
   ExpenceCateraries _expenceCatagory = ExpenceCateraries.health;
+
+  //controllers for the fields
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
+  //varible to stroe data
+  DateTime _selestedDate = DateTime.now();
+  //varible to store the time
+  DateTime _timeNow = DateTime.now();
+  //dipose the controller
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _amountController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +47,7 @@ class _AddNewScreenState extends State<AddNewScreen> {
           child: Stack(
             children: [
               Container(
-                height: MediaQuery.of(context).size.height * 0.066,
+                height: MediaQuery.of(context).size.height * 0.07,
                 decoration: BoxDecoration(
                   color: kWhite,
                   borderRadius: BorderRadius.circular(100),
@@ -118,7 +138,7 @@ class _AddNewScreenState extends State<AddNewScreen> {
                   top: MediaQuery.of(context).size.height * 0.28,
                 ),
                 width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.6,
+                height: MediaQuery.of(context).size.height * 0.68,
                 decoration: const BoxDecoration(
                   color: kWhite,
                   borderRadius: BorderRadius.only(
@@ -140,6 +160,12 @@ class _AddNewScreenState extends State<AddNewScreen> {
                             hintText: 'Category',
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(24),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kGrey.withOpacity(0.7),
+                              ),
                             ),
                           ),
                           items: _selectedMethod == 1
@@ -165,8 +191,186 @@ class _AddNewScreenState extends State<AddNewScreen> {
                                         values as ExpenceCateraries
                                   : _incomeCategory = values as IncomeCategary;
                             });
-                           
                           },
+                        ),
+                        const SizedBox(height: 24),
+                        TextFormField(
+                          controller: _titleController,
+                          decoration: InputDecoration(
+                            hintText: 'Title',
+                            hintStyle: TextStyle(color: kGrey.withOpacity(0.6)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kGrey.withOpacity(0.2),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kBlack.withOpacity(0.7),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        TextFormField(
+                          controller: _descriptionController,
+                          decoration: InputDecoration(
+                            hintText: 'Description',
+                            hintStyle: TextStyle(color: kGrey.withOpacity(0.6)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kBlack.withOpacity(0.2),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kBlack.withOpacity(0.7),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: _amountController,
+                          decoration: InputDecoration(
+                            hintText: 'Amount',
+                            hintStyle: TextStyle(color: kGrey.withOpacity(0.6)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kBlack.withOpacity(0.2),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(24),
+                              borderSide: BorderSide(
+                                color: kBlack.withOpacity(0.7),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+
+                        //data and time picker
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            //date day selector
+                            GestureDetector(
+                              onTap: () {
+                                showDatePicker(
+                                  context: context,
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2026),
+                                  // assign the selsted value to the datatime vairable
+                                ).then((vales) {
+                                  if (vales != null) {
+                                    setState(() {
+                                      _selestedDate = vales;
+                                    });
+                                  }
+                                });
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(24),
+                                  color: kMainColor,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.calendar_month),
+                                      color: kWhite,
+                                      onPressed: () {},
+                                    ),
+                                    // const SizedBox(width: 6),
+                                    const Text(
+                                      'Selected Date',
+                                      style: TextStyle(color: kWhite),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Text(
+                              DateFormat.yMMMd().format(_selestedDate),
+                              style: const TextStyle(color: kGrey),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        //time selector
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now(),
+                                ).then((val) {
+                                  if (val != null) {
+                                    setState(() {
+                                      _timeNow = DateTime(
+                                        _selestedDate.year,
+                                        _selestedDate.month,
+                                        _selestedDate.day,
+                                        val.hour,
+                                        val.minute,
+                                      );
+                                    });
+                                  }
+                                });
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(24),
+                                  color: const Color.fromARGB(
+                                    255,
+                                    245,
+                                    156,
+                                    54,
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.alarm_add),
+                                      color: kWhite,
+                                      onPressed: () {},
+                                    ),
+                                    // const SizedBox(width: 6),
+                                    const Text(
+                                      'selected Time',
+                                      style: TextStyle(color: kWhite),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Text(
+                              DateFormat.jm().format(_timeNow),
+                              style: TextStyle(color: kGrey),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 12),
+                        const Divider(color: kLightGrey, thickness: 5),
+                        const SizedBox(height: 12),
+                        CustomButton(
+                          buttonColor: _selectedMethod == 0 ? kRed : kGreen,
+                          buttonText: 'Add',
                         ),
                       ],
                     ),
